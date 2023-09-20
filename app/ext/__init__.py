702e0e11d7c6d543358d4e02682945865c21d9aa
giovanebmilani <giovanebmilani@hotmail.com>
2023-09-20 18:28:42 -0300

    socketio initialization

--- app/ext/__init__.py
+++ app/ext/__init__.py
@@ -5,6 +5,7 @@ from app.errors.base import ApiException
 from app.ext.database import db
 from app.ext.cors import cors
 from app.ext.migration import migrate
+from app.ext.socketio import socketio
 from app.ext.errorhandler import api_exception_handler
 from app.ext.modifiers import make_notfound_handling
 
@@ -17,4 +18,5 @@ def init_extensions(app: Flask):
 	migrate.init_app(app, db)
 	app.register_error_handler(ApiException, api_exception_handler)
 	cors.init_app(app)
+	socketio.init_app(app)
 	
