7e54632148e46a568bd1c43a34136f9fc8e3d561
giovanebmilani <giovanebmilani@hotmail.com>
2023-09-20 22:11:26 -0300

    adding variable to auth service

--- app/config.py
+++ app/config.py
@@ -3,12 +3,13 @@ import os
 from flask import Flask
 
 
-def getenv(name):
-    try:
-        return os.environ[name]
-    except KeyError:
-        message = "Expected environment variable '{}' not set.".format(name)
-        raise Exception(message)
+def getenv(name, required=True):
+	try:
+		return os.environ[name]
+	except KeyError:
+		if required:
+			message = "Expected environment variable '{}' not set.".format(name)
+			raise Exception(message)
 
 
 class Config:
@@ -21,6 +22,8 @@ class Config:
 
 	SQLALCHEMY_DATABASE_URI = f'{DB_ENGINE}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 	SECRET_KEY = 'Ch4v3Sup3rS3c3t4*12'
+
+	AUTH_SERVICE_API_URL = getenv('AUTH_SERVICE_API_URL', required=False)
 
 
 class DevelopmentConfig(Config):
